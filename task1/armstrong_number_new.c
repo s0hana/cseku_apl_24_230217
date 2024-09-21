1fd95c1781599cec3ef6f7d6f50d872c11b4fe8a
Sohana Rahman Tuly <168797391+s0hana@users.noreply.github.com>
2024-09-21 13:26:56 +0800

    Adding the new file as instructed

---
+++ task1/armstrong_number_new.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int number, remainder, sum=0;
+    printf("\nEnter a number:");
+    scanf("%d",&number);
+
+    int temp = number;// Storing the original number for checking later
+
+
+    // Separates all digits, cubes each digit, and adds them together to check if the sum equals the original number.
+    while(number!=0)
+    {
+        remainder=number%10;// Getting the last digit
+        number = number/10;// Removing the last digit
+       sum = sum + remainder*remainder*remainder;// Adding the cube of the digit
+    }
+
+
+    // Checking if the sum equals the original number
+    if(sum==temp)
+        printf("\nThe number is  armstrong.\n");
+    else
+        printf("\nThe number is  not armstrong.\n");
+    return 0;
+}
